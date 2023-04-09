f7d6ff28ef510b134ea71347d844848fce9347e2
Kevin Armbruster <KevinArmbruster2013@gmail.com>
2023-04-09 11:24:08 +0200

    fix learning

--- A2/Assignment2.py
+++ A2/Assignment2.py
@@ -42,103 +42,142 @@ def normalize(X: np.ndarray, mean: np.ndarray, std: np.ndarray):
     return X
 
 
-def ForwardPass(X: np.ndarray, params) -> dict:
-    s1 = params["W1"] @ X + params["b1"]
-    h = np.maximum(0, s1)
-    s2 = params["W2"] @ h + params["b2"]
+def ForwardPass(X: np.ndarray, layers: list, cache=False) -> np.ndarray:
+
+    s1 = layers[0]["W"] @ X + layers[0]["b"]
+    h = relu(s1)
+
+    s2 = layers[1]["W"] @ h + layers[1]["b"]
     pred = softmax(s2)
 
-    assert h.shape == (params["W1"].shape[0], X.shape[1])
-    assert pred.shape == (params["W2"].shape[0], X.shape[1])
+    assert h.shape == (layers[0]["W"].shape[0], X.shape[1])
+    assert pred.shape == (layers[1]["W"].shape[0], X.shape[1])
 
-    activation = {"h": h, "p": pred}
-    return activation
+    if cache:
+        layers[0]["activation"] = h
+        layers[1]["activation"] = pred
 
-
-def ComputeCost(X: np.ndarray, Y: np.ndarray, params: dict, lmbda: float) -> tuple[float, float]:
-    act = ForwardPass(X, params)
-    N = act["p"].shape[1]
-    ce_loss = -np.sum(Y * np.log(act["p"])) / N
-    cost = ce_loss + 2 * lmbda * np.sum(params["W1"]) \
-                   + 2 * lmbda * np.sum(params["W2"])
-    return cost, ce_loss
+    return pred
 
 
-def ComputeAccuracy(X: np.ndarray, y: np.ndarray, params: dict) -> float:
-    act = ForwardPass(X, params)
-    pred = np.argmax(act["p"], axis=0)
-    acc = np.mean(y == pred)
+def relu(X, d=False):
+    if not d:
+        return np.maximum(0, X)
+        # np.where(X > 0, X, 0)
+    else:
+        return np.where(X > 0, 1, 0)
+
+
+def ComputeCost(X: np.ndarray, Y: np.ndarray, layers: list, lmbda: float) -> tuple[float, float]:
+    pred = ForwardPass(X, layers)
+    N = pred.shape[1]
+    loss_cross = -np.sum(Y * np.log(pred)) / N
+    loss_cross222 = sum(-np.log((Y * pred).sum(axis=0))) / N
+    assert np.allclose(loss_cross222, loss_cross)
+
+    loss_regularization = sum([lmbda * (layer["W"] ** 2).sum() for layer in layers])
+    cost = loss_cross + loss_regularization
+    return cost, loss_cross
+
+
+def ComputeAccuracy(X: np.ndarray, y: np.ndarray, layers: list) -> float:
+    pred = ForwardPass(X, layers)
+    p = np.argmax(pred, axis=0)
+    acc = np.mean(y == p)
     return acc
 
 
-def ComputeGradsNum(X, Y, params, lmbda, h=1e-5):
-    W = [params["W1"], params["W2"]]
-    b = [params["b1"], params["b2"]]
-    # Initialize gradients
-    grad_W = [np.zeros_like(Wi) for Wi in W]
-    grad_b = [np.zeros_like(bi) for bi in b]
+def ComputeGradsNum(X, Y, layers, lambda_, h=1e-5):
+    Ws = [layers[0]["W"], layers[1]["W"]]
+    bs = [layers[0]["b"], layers[1]["b"]]
 
-    # Compute cost with current parameters
-    c, _ = ComputeCost(X, Y, params, lmbda)
+    grad_W = [np.zeros_like(Wl) for Wl in Ws]
+    grad_b = [np.zeros_like(bl) for bl in bs]
 
-    # Compute gradients numerically for each parameter
-    for j in range(len(b)):
-        grad_b[j] = np.zeros_like(b[j])
+    c, _ = ComputeCost(X, Y, layers, lambda_)
 
-        for i in range(len(b[j])):
-            b_try = b.copy()
+    for j in range(len(bs)):
+        for i in range(bs[j].size):
+            b_try = copy.deepcopy(bs)
             b_try[j][i] += h
-            tmp = buildParamsDict(params["W1"], b_try[0], params["W2"], b_try[1])
-            c2, _ = ComputeCost(X, Y, tmp, lmbda)
+            tmp = BuildTmpLayers([layers[0]["W"], layers[1]["W"]], b_try)
+            c2, _ = ComputeCost(X, Y, tmp, lambda_)
             grad_b[j][i] = (c2 - c) / h
 
-    for j in range(len(W)):
-        grad_W[j] = np.zeros_like(W[j])
+    for j in range(len(Ws)):
+        for i in np.ndindex(Ws[j].shape):
+            W_try = copy.deepcopy(Ws)
+            W_try[j][i] += h
+            tmp = BuildTmpLayers(W_try, [layers[0]["b"], layers[1]["b"]])
+            c2, _ = ComputeCost(X, Y, tmp, lambda_)
+            grad_W[j][i] = (c2 - c) / h
 
-        for i in range(W[j].size):
-            W_try = W.copy()
-            W_try[j].flat[i] += h
-            tmp = buildParamsDict(W_try[0], params["b1"], W_try[1], params["b2"])
+    return grad_W, grad_b
+
+
+def ComputeGradsNumGit(X, Y, layers, lmbda, h=1e-5):
+    W2 = layers[1]["W"]
+    b2 = layers[1]["b"]
+    W1 = layers[0]["W"]
+    b1 = layers[0]["b"]
+    grad_W2 = np.zeros(shape=W2.shape)
+    grad_b2 = np.zeros(shape=b2.shape)
+    grad_W1 = np.zeros(shape=W1.shape)
+    grad_b1 = np.zeros(shape=b1.shape)
+    c, _ = ComputeCost(X, Y, layers, lmbda)
+
+    for i in range(b1.shape[0]):
+        b1_try = b1.copy()
+        b1_try[i, 0] += h
+        tmp = BuildTmpLayers([layers[0]["W"], layers[1]["W"]], [b1_try, b2])
+        c2, _ = ComputeCost(X, Y, tmp, lmbda)
+        grad_b1[i, 0] = (c2 - c) / h
+
+    for i in range(W1.shape[0]):
+        for j in range(W1.shape[1]):
+            W1_try = W1.copy()
+            W1_try[i, j] += h
+            tmp = BuildTmpLayers([W1_try, W2], [layers[0]["b"], layers[1]["b"]])
             c2, _ = ComputeCost(X, Y, tmp, lmbda)
-            grad_W[j].flat[i] = (c2 - c) / h
+            grad_W1[i, j] = (c2 - c) / h
 
-    return grad_b, grad_W
+    for i in range(b2.shape[0]):
+        b2_try = b2.copy()
+        b2_try[i, 0] += h
+        tmp = BuildTmpLayers([layers[0]["W"], layers[1]["W"]], [b1, b2_try])
+        c2, _ = ComputeCost(X, Y, tmp, lmbda)
+        grad_b2[i, 0] = (c2 - c) / h
 
+    for i in range(W2.shape[0]):
+        for j in range(W2.shape[1]):
+            W2_try = W2.copy()
+            W2_try[i, j] += h
+            tmp = BuildTmpLayers([W1, W2_try], [layers[0]["b"], layers[1]["b"]])
+            c2, _ = ComputeCost(X, Y, tmp, lmbda)
+            grad_W2[i, j] = (c2 - c) / h
 
-def ComputeGradients(X: np.ndarray, Y: np.ndarray, activation: dict, params: dict, lmbda: float) -> dict:
-    N = X.shape[1]
-
-    error_out = activation["p"] - Y
-
-    grad_W2 = (1 / N) * error_out @ activation["h"].T
-    grad_b2 = (1 / N) * np.sum(error_out, axis=1, keepdims=True)
-
-    error_hidden = params["W2"].T @ error_out
-    hidden_activation_derivative = activation["h"] * (1 - activation["h"])
-    error_hidden = np.multiply(error_hidden, hidden_activation_derivative)
-
-    grad_W1 = (1 / N) * error_hidden @ X.T
-    grad_b1 = (1 / N) * np.sum(error_hidden, axis=1, keepdims=True)
-
-    # add regularization derivative
-    if lmbda != 0:
-        grad_W1 += 2 * lmbda * params["W1"]
-        grad_W2 += 2 * lmbda * params["W2"]
-
-    assert grad_W1.shape == params["W1"].shape
-    assert grad_W2.shape == params["W2"].shape
-    assert grad_b1.shape == params["b1"].shape
-    assert grad_b2.shape == params["b2"].shape
-
-    grads = {"grad_W2": grad_W2, "grad_b2": grad_b2, "grad_W1": grad_W1, "grad_b1": grad_b1}
-    return grads
+    return grad_W2, grad_b2, grad_W1, grad_b1
 
 
-def getBatchDumb(X, Y, y, batchSize=100):
-    X_batch = X[:, :batchSize]
-    Y_batch = Y[:, :batchSize]
-    y_batch = y[:batchSize]
-    return X_batch, Y_batch, y_batch
+def ComputeGradients(X: np.ndarray, Y: np.ndarray, layers: list, lmbda: float):
+
+    error = layers[1]["activation"] - Y
+
+    N = layers[1]["activation"].shape[1]
+    layers[1]["dW"] = (1 / N) * error @ layers[0]["activation"].T + 2 * lmbda * layers[1]["W"]
+    layers[1]["db"] = np.mean(error, axis=1, keepdims=True)
+
+    error = layers[1]["W"].T @ error
+    dHidden = error * relu(layers[0]["activation"], d=True)
+
+    layers[0]["dW"] = (1 / N) * dHidden @ X.T + 2 * lmbda * layers[0]["W"]
+    layers[0]["db"] = np.mean(dHidden, axis=1, keepdims=True)
+
+    assert layers[0]["dW"].shape == layers[0]["W"].shape
+    assert layers[1]["dW"].shape == layers[1]["W"].shape
+    assert layers[0]["db"].shape == layers[0]["b"].shape
+    assert layers[1]["db"].shape == layers[1]["b"].shape
+    return
 
 
 def YieldMiniBatch(_X, _Y, _y, batchSize=100):
@@ -177,7 +216,7 @@ def plot_cost_and_accuracy(history):
     ax2 = ax1.twinx()
     color = 'tab:blue'
     ax2.set_ylabel('Accuracy', color=color)
-    ax2.plot(range(1, len(history["accuracies"])+1), history["accuracies"], color=color, label=f"""Accuracy (final: {history["val_accuracies"][-1]:.2f})""")
+    ax2.plot(range(1, len(history["accuracies"])+1), history["accuracies"], color=color, label=f"""Accuracy (final: {history["accuracies"][-1]:.2f})""")
     ax2.plot(range(1, len(history["val_accuracies"])+1), history["val_accuracies"], color="cyan", label=f"""Val Accuracy (final: {history["val_accuracies"][-1]:.2f})""")
     ax2.tick_params(axis='y', labelcolor=color)
 
@@ -194,8 +233,8 @@ def plot_cost_and_accuracy(history):
     plt.show()
 
 
-def plot_weights(W):
-    fig, axs = plt.subplots(2, 5, figsize=(10, 4))
+def plot_weights(W, max_images=10):
+    fig, axs = plt.subplots(2, 5, figsize=(max_images, 4))
 
     for i in range(len(W)):
         im = np.reshape(W[i, :], (32, 32, 3))
@@ -206,76 +245,87 @@ def plot_weights(W):
         axs[i // 5, i % 5].axis('off')
         axs[i // 5, i % 5].set_title(f'Weight {i + 1}')
 
+        if i >= max_images-1:
+            break
+
     plt.show()
 
 
-def initNNParams(hidden_nodes):
-    params = {}
-    params["W1"] = np.random.normal(0, 1 / np.sqrt(D), (hidden_nodes, D))
-    params["b1"] = np.random.normal(0, 1 / np.sqrt(hidden_nodes), (hidden_nodes, 1))
+def InitializeLayers(hidden_nodes: list) -> list:
+    layers = []
+    for i in range(1, len(hidden_nodes)):
+        fan_in = hidden_nodes[i - 1]
+        fan_out = hidden_nodes[i]
 
-    params["W2"] = np.zeros((K, hidden_nodes))
-    params["b2"] = np.zeros((K, 1))
-    return params
+        W = np.random.normal(0, 1 / np.sqrt(fan_in), (fan_out, fan_in))
+        b = np.zeros((fan_out, 1))
 
+        layers.append({"W": W, "b": b})
 
-def buildParamsDict(W1, b1, W2, b2):
-    return {"W1": W1, "b1": b1, "W2": W2, "b2": b2}
-
-
-def updateNNParams(params, grads, hyper):
-    params["W1"] -= hyper["lr"] * grads["grad_W1"]
-    params["b1"] -= hyper["lr"] * grads["grad_b1"]
-
-    params["W2"] -= hyper["lr"] * grads["grad_W2"]
-    params["b2"] -= hyper["lr"] * grads["grad_b2"]
-    return params
+    return layers
 
 
-def TrainCLF(params, hyper, X_train, Y_train, y_train, X_val, Y_val, y_val):
+def BuildTmpLayers(Ws: list, bs: list) -> list:
+    f = {"W": Ws[0], "b": bs[0]}
+    s = {"W": Ws[1], "b": bs[1]}
+    return [f, s]
+
+
+def SGDStep(layers, lr):
+    for layer in layers:
+        layer["W"] = layer["W"] - lr * layer["dW"]
+        layer["b"] = layer["b"] - lr * layer["db"]
+    return layers
+
+
+def TrainCLF(layers, hyper, X_train, Y_train, y_train, X_val, Y_val, y_val):
     epochs = hyper["epochs"]
     batchesPerEpoch = np.ceil(X_train.shape[1] / hyper["batchSize"]).astype(int)
     history = defaultdict(list)
 
     for j in range(epochs):
         for i, X_batch, Y_batch, y_batch in YieldMiniBatch(X_train, Y_train, y_train, hyper["batchSize"]):
-            activation = ForwardPass(X_batch, params)
+            # dim = 1000
+            # X_batch = X_batch[:dim,]
+            # layers = InitializeLayers([dim, 50, 10])
 
-            grads = ComputeGradients(X_batch, Y_batch, activation, params, hyper["l2"])
+            pred = ForwardPass(X_batch, layers, cache=True)
 
-            grad_b_num, grad_W_num = ComputeGradsNum(X_batch, Y_batch, params, hyper["l2"])
+            ComputeGradients(X_batch, Y_batch, layers, hyper["l2"])
 
-            assert np.allclose(grads["grad_b1"], grad_b_num[0], atol=1e-4)
-            assert np.allclose(grads["grad_W1"], grad_W_num[0], atol=1e-4)
+            # grad_W, grad_b = ComputeGradsNum(X_batch, Y_batch, layers, hyper["l2"])
+            #
+            # assert np.allclose(grad_b[1], layers[1]["db"], atol=1e-5)
+            # assert np.allclose(grad_W[1], layers[1]["dW"], atol=1e-5)
+            #
+            # assert np.allclose(grad_b[0], layers[0]["db"], atol=1e-5)
+            # assert np.allclose(grad_W[0], layers[0]["dW"], atol=1e-5)
 
-            assert np.allclose(grads["grad_b2"], grad_b_num[1], atol=1e-4)
-            assert np.allclose(grads["grad_W2"], grad_W_num[1], atol=1e-4)
+            SGDStep(layers, hyper["lr"])
 
-            updateNNParams(params, grads, hyper)
+        # Train data
+        cost, loss = ComputeCost(X_train, Y_train, layers, hyper["l2"])
+        acc = ComputeAccuracy(X_train, y_train, layers)
+        history["costs"].append(cost)
+        history["losses"].append(loss)
+        history["accuracies"].append(acc)
 
-            # Train data
-            cost, loss = ComputeCost(X_train, Y_train, params, hyper["l2"])
-            acc = ComputeAccuracy(X_train, y_train, params)
-            history["costs"].append(cost)
-            history["losses"].append(loss)
-            history["accuracies"].append(acc)
+        # Val data
+        cost_val, loss_val = ComputeCost(X_val, Y_val, layers, hyper["l2"])
+        acc_val = ComputeAccuracy(X_val, y_val, layers)
+        history["val_costs"].append(cost_val)
+        history["val_losses"].append(loss_val)
+        history["val_accuracies"].append(acc_val)
 
-            # Val data
-            cost_val, loss_val = ComputeCost(X_val, Y_val, params, hyper["l2"])
-            acc_val = ComputeAccuracy(X_val, y_val, params)
-            history["val_costs"].append(cost_val)
-            history["val_losses"].append(loss_val)
-            history["val_accuracies"].append(acc_val)
+        print(f"""Epoch {j + 1}/{epochs}: Batch {i + 1}/{batchesPerEpoch}: Cost={cost:8.4f} ; Acc={acc:8.4f} ; Val Cost={cost_val:8.4f} ; Val Acc={acc_val:8.4f}""")
 
-            print(f"""Epoch {j + 1}/{epochs}: Batch {i + 1}/{batchesPerEpoch}: Cost={cost} ; Acc={acc} ; Val Cost={cost_val} ; Val Acc={acc_val}""")
-
-    return params, history
+    return layers, history
 
 
 D = 3072  # dimensionality
 N = 10000  # samples
 K = 10  # classes
-hidden_nodes = 50
+hidden_nodes = [D, 50, K]
 
 X_train, Y_train, y_train = load_batch("data_batch_1")
 # helper.montage(X_train)
@@ -298,25 +348,25 @@ X_train = normalize(X_train, Xmean, Xstd)
 X_val = normalize(X_val, Xmean, Xstd)
 X_test = normalize(X_test, Xmean, Xstd)
 
-params = initNNParams(hidden_nodes)
+layers = InitializeLayers(hidden_nodes)
 
 hyper = {
     "epochs": 40,
-    "batchSize": 5,
-    "l2": 0,
-    "lr": .001,
+    "batchSize": 100,
+    "l2": 0.1,
+    "lr": 0.001,
 }
 
 
-params, history = TrainCLF(params, hyper, X_train, Y_train, y_train, X_val, Y_val, y_val)
+layers, history = TrainCLF(layers, hyper, X_train, Y_train, y_train, X_val, Y_val, y_val)
 
 plot_cost_and_accuracy(history)
 
 # Test data
-acc_test = ComputeAccuracy(X_test, y_test, params)
+acc_test = ComputeAccuracy(X_test, y_test, layers)
 
-plot_weights(params["W1"])
-plot_weights(params["W2"])
+plot_weights(layers[0]["W"])
+# plot_weights(layers[1]["W"])
 
 
 print(f"Final test acc {acc_test}")
